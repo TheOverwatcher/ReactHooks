52045376c954891c7c7e5f0e762b2ad93dee335e
TheOverwatcher <graysonbonds@gmail.com>
2021-01-07 11:27:38 -0500

    Create our own hook.
    Rather than continuously writing out useState for each parameter in, say,
    a form we can do so with our own hook. The useForm hook sets each value to
    the initial value provided and returns the values object. The setValues method
    sets all given values that haven't changed plus the target value that does change.
    onChange is assigned handleChange which then is assigned the setValues method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React, {useState} from 'react';
 import './App.scss';
+import {useForm} from './useForm';
 
 function initialState() {
     return {count:10, count2:20};
 }
 
 const App = () => { 
-  const [count3, setCount3] = useState(30);
-  const [{count, count2}, setCount] = useState(() => initialState());
+//   const [count3, setCount3] = useState(30);
+//   const [{count, count2}, setCount] = useState(() => initialState());
+    const [values, handleChange] = useForm({email:'', password:''});
 
   return (
     <div>
-        <button onClick={() => {
+        {/* <button onClick={() => {
             setCount(currentState => ({...currentState, count: currentState.count + 1}))
             setCount3(c3 => c3 + 1)
         }
             }>+</button>
         <div>count 1:{count}</div>
         <div>count 2:{count2}</div>
-        <div>count 3:{count2}</div>
+        <div>count 3:{count3}</div> */}
+
+        <input name='email' value={values.email} onChange={handleChange}></input>
+        <input type='password' name='password' value={values.password} onChange={handleChange}></input>
     </div>
   );
 }
