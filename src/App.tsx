c5d401313760337dc97734c1e6fd37a3cfaf1d31
TheOverwatcher <graysonbonds@gmail.com>
2021-01-07 10:16:28 -0500

    Add basic useState implementation.
    Hooks can only be used in functions. React.useState can be used
    multiple times in a function and set to either a function or a value.
    useState returns an array of the value, and method to change the value.
    THe method to change the value can simplistically change the state of one
    or more properties of an object, depending on what useState sets initially.
    Object should have their properties deconstructed '{}' in order to use values.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 
-function App() {
+function initialState() {
+    return {count:10, count2:20};
+}
+
+const App = () => { 
+  const [count3, setCount3] = useState(30);
+  const [{count, count2}, setCount] = useState(() => initialState());
+
   return (
-    <div>hey</div>
+    <div>
+        <button onClick={() => {
+            setCount(currentState => ({...currentState, count: currentState.count + 1}))
+            setCount3(c3 => c3 + 1)
+        }
+            }>+</button>
+        <div>count 1:{count}</div>
+        <div>count 2:{count2}</div>
+        <div>count 3:{count2}</div>
+    </div>
   );
 }
 
